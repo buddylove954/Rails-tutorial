9747a9fc7c49d45174bb6590d3f5ff32f32f72b1
will love <wlove29@gmail.com>
2015-04-06 15:50:00 -0400

    more indepth sqlite training ;p

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406161141) do
+ActiveRecord::Schema.define(version: 20150406175403) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "descriptions"
   end
 
 end
